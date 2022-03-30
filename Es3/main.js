f8c81357a22aacbb076eb1d70db3051a38ec8229
Aurel1701 <gantzfan96@gmail.com>
2022-03-30 11:53:39 +0200

    edit

--- Es3/main.js
+++ Es3/main.js
@@ -1,9 +1,10 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
-let numbers = Number(prompt ('digita un numero'))
+
 
 
 for (let i = 0; i < 11; i++) {
+    let numbers = Number(prompt ('digita un numero'))
   numbers = numbers + i;
   console.log(numbers);
 }
