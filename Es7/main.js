8803f16e3268ff31ea1eae2d06ef567cfedb0e98
Aurel1701 <gantzfan96@gmail.com>
2022-03-30 15:52:42 +0200

    edit

---
+++ Es7/main.js
@@ -0,0 +1,15 @@
+//Stampa le potenze di 2 fino a 1000.
+
+let potenza = 2
+
+for (let i = 0; i < 1000; i++) {
+    
+    if (potenza < 1000) {
+        potenza = potenza * 2
+        console.log(potenza);
+
+    }
+
+
+
+}
