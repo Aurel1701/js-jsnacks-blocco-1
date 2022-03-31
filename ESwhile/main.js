9142ea86a95d884dd0a48b529cbd92189dd495e6
Aurel1701 <gantzfan96@gmail.com>
2022-03-31 12:38:55 +0200

    edit

---
+++ ESwhile/main.js
@@ -0,0 +1,21 @@
+
+
+
+const x = 44
+
+switch (true) {
+    case x > 10:
+        console.log('è vero');
+
+    case x < 10:
+        console.log('è falso');
+
+    case x === 10:
+        console.log('è falso');
+        
+        break;
+
+    default:
+        break;
+}
+
