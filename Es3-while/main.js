26088d25ff803001267b7230b398ed232ea825db
Aurel1701 <gantzfan96@gmail.com>
2022-03-31 11:48:10 +0200

    edit

---
+++ Es3-while/main.js
@@ -0,0 +1,14 @@
+
+let somma, numbers
+
+somma = 0
+let i = 0
+
+while (i < 10) {
+  let numbers = Number(prompt ('digita un numero'))
+  somma = numbers + somma;
+  i++
+  
+    
+}
+console.log(somma);
